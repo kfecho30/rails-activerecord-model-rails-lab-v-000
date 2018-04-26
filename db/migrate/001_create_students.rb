d0b69a510c5fd07a172842b4631127662140b123
Kelsey Fecho <kfecho30@gmail.com>
2018-04-26 13:24:43 +0000

    automatically backed up by learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,8 +1,8 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |s|
-      t.string :first_name
-      t.string :last_name
+      s.string :first_name
+      s.string :last_name
     end
   end
 end
